d44456e2c2dc90bbbde69916978c5f23ab8bbcfc
Wisam Ahmad <wisamahmad786@gmail.com>
2024-11-30 21:32:37 +0500

    navbar transition completed

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -63,64 +63,67 @@ const Sidebar = () => {
         {isOpen ? "" : <Menu className="w-6 h-6" />}
       </button>
 
-      {/* Overlay and Sidebar for Mobile */}
-      {isOpen && (
-        <>
-          {/* Overlay */}
-          <div
-            className="fixed inset-0 bg-black bg-opacity-50 z-40"
-            onClick={() => setIsOpen(false)}
-          ></div>
+      {/* Overlay */}
+      <div
+        className={`fixed inset-0 bg-black z-40 transition-opacity duration-700 ${
+          isOpen
+            ? "opacity-50 pointer-events-auto"
+            : "opacity-0 pointer-events-none"
+        }`}
+        onClick={() => setIsOpen(false)}
+      ></div>
 
-          {/* Sidebar */}
-          <div className="fixed top-0 left-0 flex flex-col w-[300px] min-w-[300px] gap-2 border-r min-h-screen p-4 bg-slate-700 z-50 opacity-90">
-            <div className="flex ">
-              <Image
-                src={Logo}
-                alt="Logo"
-                width={200}
-                height={100}
-                className="p-2"
-              />
-              {/* Close Button */}
-              <button
-                className="md:hidden p-2 m-2 text-slate-700"
-                onClick={() => setIsOpen(false)}
-                aria-label="Close Menu"
-              >
-                <X className="w-6 h-6 text-white" />
-              </button>
-            </div>
-            <div className="grow ">
-              <Command style={{ overflow: "visible" }}>
-                <CommandList style={{ overflow: "visible" }}>
-                  {menuList.map((items: MenuItem, key: number) => (
-                    <CommandGroup key={key}>
-                      <CommandItem className="flex gap-2 cursor-pointer ">
-                        {items.icon}
-                        {items.text}
-                      </CommandItem>
-                    </CommandGroup>
-                  ))}
-                </CommandList>
-              </Command>
-            </div>
-            <div>
-              <Command style={{ overflow: "visible", paddingBottom: "8px" }}>
-                <CommandList style={{ overflow: "visible" }}>
-                  {BelowMenuList.map((items: BelowMenuItem, key: number) => (
-                    <CommandGroup key={key}>
-                      <CommandItem className="flex gap-2 cursor-pointer pb-2 justify-center">
-                        {items.text}
-                      </CommandItem>
-                    </CommandGroup>
-                  ))}
-                </CommandList>
-              </Command>
-            </div>
-          </div>
-        </>
-      )}
+      {/* Sidebar */}
+      <div
+        className={`fixed top-0 left-0 transform ${
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        } duration-500 flex flex-col w-[300px] min-w-[300px] gap-2 border-r min-h-screen p-4 bg-slate-700 z-50 opacity-90`}
+      >
+        <div className="flex">
+          <Image
+            src={Logo}
+            alt="Logo"
+            width={200}
+            height={100}
+            className="p-2"
+          />
+          {/* Close Button */}
+          <button
+            className="md:hidden p-2 m-2 text-slate-700"
+            onClick={() => setIsOpen(false)}
+            aria-label="Close Menu"
+          >
+            <X className="w-6 h-6 text-white" />
+          </button>
+        </div>
+        <div className="grow ">
+          <Command style={{ overflow: "visible" }}>
+            <CommandList style={{ overflow: "visible" }}>
+              {menuList.map((items: MenuItem, key: number) => (
+                <CommandGroup key={key}>
+                  <CommandItem className="flex gap-2 cursor-pointer ">
+                    {items.icon}
+                    {items.text}
+                  </CommandItem>
+                </CommandGroup>
+              ))}
+            </CommandList>
+          </Command>
+        </div>
+        <div>
+          <Command style={{ overflow: "visible", paddingBottom: "8px" }}>
+            <CommandList style={{ overflow: "visible" }}>
+              {BelowMenuList.map((items: BelowMenuItem, key: number) => (
+                <CommandGroup key={key}>
+                  <CommandItem className="flex gap-2 cursor-pointer pb-2 justify-center">
+                    {items.text}
+                  </CommandItem>
+                </CommandGroup>
+              ))}
+            </CommandList>
+          </Command>
+        </div>
+      </div>
 
       {/* Sidebar for Desktop */}
       <div
